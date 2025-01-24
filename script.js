01de7458af9b865c70489536ec14b41566b53d7e
eadwardandrei <167729096+eadwardandrei@users.noreply.github.com>
2025-01-24 18:19:42 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+function openForm1() {
+    document.getElementById("myForm1").style.display = "block";
+  }
+  
+  function closeForm1() {
+    document.getElementById("myForm1").style.display = "none";
+  }
+
+  function openForm2() {
+    document.getElementById("myForm2").style.display = "block";
+  }
+  
+  function closeForm2() {
+    document.getElementById("myForm2").style.display = "none";
+  }
+
+  function openForm3() {
+    document.getElementById("myForm3").style.display = "block";
+  }
+  
+  function closeForm3() {
+    document.getElementById("myForm3").style.display = "none";
+  }
+
+  function openForm4() {
+    document.getElementById("myForm4").style.display = "block";
+  }
+  
+  function closeForm4() {
+    document.getElementById("myForm4").style.display = "none";
+  }
+
+  function openForm5() {
+    document.getElementById("myForm4").style.display = "block";
+  }
+  
+  function closeForm5() {
+    document.getElementById("myForm4").style.display = "none";
+  }
+
+  function openForm6() {
+    document.getElementById("myForm4").style.display = "block";
+  }
+  
+  function closeForm6() {
+    document.getElementById("myForm4").style.display = "none";
+  }
+
+
+  
